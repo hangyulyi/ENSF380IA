c3106fe7180820a4a4fd97f62af74ad4bd0d54c4
Hangyul Yi <hangyulyi3@gmail.com>
2024-02-27 01:27:08 -0700

    add initial documentation

--- edu/ucalgary/oop/Location.java
+++ edu/ucalgary/oop/Location.java
@@ -1,3 +1,9 @@
+/**
+ * @author  Hangyul Yi
+ * @version 1.0
+ * @since   1.0
+ */
+
 package edu.ucalgary.oop;
 
 public class Location {
---
+++ edu/ucalgary/oop/Supply.java
@@ -0,0 +1,11 @@
+/**
+ * @author  Hangyul Yi
+ * @version 1.0
+ * @since   1.0
+ */
+
+package edu.ucalgary.oop;
+
+public class Supply {
+   
+}
