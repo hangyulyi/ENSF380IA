85a807bf4a5f9280d1da9e889bd21a631fb58364
Hangyul Yi <hangyulyi3@gmail.com>
2024-03-11 01:02:48 -0600

    set FamilyRelation class

--- edu/ucalgary/oop/FamilyRelation.java
+++ edu/ucalgary/oop/FamilyRelation.java
@@ -1,11 +1,43 @@
 /**
  * @author  Hangyul Yi
- * @version 1.0
+ * @version 1.1
  * @since   1.0
  */
 
 package edu.ucalgary.oop;
 
+/**
+ * Represents a FamilyRelation between two DisasterVictims
+ * 
+ * User can get/set relationships
+ */
 public class FamilyRelation {
-   
+   private DisasterVictim personOne;
+   private String relationshipTo;
+   private DisasterVictim personTwo;
+
+   /* Constructor */
+   public FamilyRelation(DisasterVictim personOne, String relationshipTo, DisasterVictim personTwo) {
+      this.personOne = personOne;
+      this.relationshipTo = relationshipTo;
+      this.personTwo = personTwo;
+   }
+
+   /* Setters */
+   public void setPersonOne(DisasterVictim personOne) {
+      this.personOne = personOne;
+   }
+
+   public void setRelationshipTo(String relationshipTo) {
+      this.relationshipTo = relationshipTo;
+   }
+
+   public void setPersonTwo(DisasterVictim personTwo) {
+      this.personTwo = personTwo;
+   }
+
+   /* Getters */
+   public DisasterVictim getPersonOne() { return this.personOne; }
+   public String getRelationshipTo() { return this.relationshipTo; }
+   public DisasterVictim getPersonTwo() { return this.personTwo; }
 }
