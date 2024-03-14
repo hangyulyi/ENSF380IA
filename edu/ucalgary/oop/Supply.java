02e927e857634ebcc7a116047b6ca25556304701
Hangyul Yi <hangyulyi3@gmail.com>
2024-03-14 02:02:09 -0600

    update Supply class

--- edu/ucalgary/oop/Supply.java
+++ edu/ucalgary/oop/Supply.java
@@ -6,21 +6,35 @@
 
 package edu.ucalgary.oop;
 
+import java.util.Objects;
+
 public class Supply {
    private String type;
    private int quantity;
    private boolean available;
 
    /* Constructor */
-   public Supply(String type, int quantity) {
+   public Supply(String type, int quantity) throws IllegalArgumentException {
       this.type = type;
+      if(quantity < 0) {
+         throw new IllegalArgumentException("Quantity cannot be negative");
+      }
       this.quantity = quantity;
-      if(quantity > 0) {
-         this.available = true;
-      }
-      else {
-         this.available = false;
-      }
+      this.available = quantity > 0;
+   }
+
+   /* Override equals() and hashCode() */
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      Supply supply = (Supply) o;
+      return quantity == supply.quantity && available == supply.available && Objects.equals(type, supply.type);
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hash(type, quantity, available);
    }
 
    /* Getters and setters */
@@ -36,8 +50,12 @@ public class Supply {
       return quantity;
    }
 
-   public void setQuantity(int quantity) {
+   public void setQuantity(int quantity) throws IllegalArgumentException {
+      if (quantity < 0) {
+         throw new IllegalArgumentException("Quantity cannot be negative");
+      }
       this.quantity = quantity;
+      this.available = quantity > 0;
    }
 
    public boolean isAvailable() {
