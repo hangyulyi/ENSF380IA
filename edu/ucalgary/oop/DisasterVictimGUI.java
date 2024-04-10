9a055d812f88eb7e8fd9a8bedc9d0cd63d9e6d2a
Hangyul Yi <hangyulyi3@gmail.com>
2024-04-09 20:13:54 -0600

    stuff kind of works what the

--- edu/ucalgary/oop/DisasterVictimGUI.java
+++ edu/ucalgary/oop/DisasterVictimGUI.java
@@ -26,10 +26,30 @@ public class DisasterVictimGUI extends JFrame implements ActionListener, MouseLi
    private ArrayList<DisasterVictim> disasterVictims;
    private JList<String> victimList;
 
+   private DefaultListModel<String> listModel;
+
+   private JTextField fnInput, lnInput, entryInput, ageOrDOBInput;
+   private JComboBox<String> genderComboBox;
+   private JComboBox<DietaryRestrictions> dietComboBox;
+   private JTextArea commentInput;
+   private JTextArea medicalRecordsTextArea;
+   private JTextField relativeInput, relationshipInput;
+   private JButton addFamilyConnectionButton;
+
+
+   private JLabel fnLabel;
+   private JLabel lnLabel;
+   private JLabel entryLabel;
+   private JLabel ageOrDOBLabel;
+   private JLabel genderLabel;
+   private JLabel dietLabel;
+   private JLabel commentLabel;
+   private JCheckBox dietCheckBox;
+
    private JLabel instructions;
    private JButton submitButton;
 
-   private VictimInfoPanel victimInfoPanel;
+
    private JPanel cardPanel;
    private CardLayout cardLayout;
 
@@ -51,6 +71,124 @@ public class DisasterVictimGUI extends JFrame implements ActionListener, MouseLi
 
       showHomePage();
       setVisible(true);
+   }
+
+   /**
+    * Create Victim Info Panel that displays all information user can edit on a Disaster Victim
+    */
+   private JPanel VictimInfoPanel() {
+      fnLabel = new JLabel("First name:");
+      lnLabel = new JLabel("Last Name:");
+      entryLabel = new JLabel("Entry Date:");
+      ageOrDOBLabel = new JLabel("Age / Date of birth:");
+      genderLabel = new JLabel("Gender:");
+      dietLabel = new JLabel("Dietary Restrictions:");
+      commentLabel = new JLabel("Additional comments:");
+
+      dietCheckBox = new JCheckBox();
+      dietCheckBox.addActionListener(e -> dietComboBox.setEnabled(dietCheckBox.isSelected()));
+
+      fnInput = new JTextField(10);
+      lnInput = new JTextField(10);
+      entryInput = new JTextField(10);
+      ageOrDOBInput = new JTextField(10);
+      genderComboBox = new JComboBox<>();
+      dietComboBox = new JComboBox<>(DietaryRestrictions.values());
+      commentInput = new JTextArea(5, 10);
+      medicalRecordsTextArea = new JTextArea(5, 10);
+      relativeInput = new JTextField(10);
+      relationshipInput = new JTextField(10);
+      addFamilyConnectionButton = new JButton("Add Family Connection");  
+
+      JPanel headerPanel = new JPanel(new BorderLayout());
+      headerPanel.add(new JLabel("Disaster Victim Information"), BorderLayout.CENTER);
+
+      JPanel inputPanel = new JPanel(new GridBagLayout());
+      GridBagConstraints gbc = new GridBagConstraints();
+      gbc.anchor = GridBagConstraints.WEST;
+      gbc.insets = new Insets(5, 5, 5, 5);
+      gbc.gridx = 0;
+      gbc.gridy = 0;
+      gbc.gridwidth = 2;
+
+      // Add labels and inputs for basic victim info
+      gbc.gridwidth = 1;
+
+      gbc.gridy++;
+      inputPanel.add(fnLabel, gbc);
+      gbc.gridy++;
+      inputPanel.add(lnLabel, gbc);
+      gbc.gridy++;
+      inputPanel.add(entryLabel, gbc);
+      gbc.gridy++;
+      inputPanel.add(ageOrDOBLabel, gbc);
+      gbc.gridy++;
+      inputPanel.add(genderLabel, gbc);
+      gbc.gridy++;
+  
+      inputPanel.add(dietLabel, gbc);
+      gbc.gridy++;
+      inputPanel.add(commentLabel, gbc);
+
+      gbc.gridx = 1;
+      gbc.gridy = 1;
+      inputPanel.add(fnInput, gbc);
+      gbc.gridy++;
+      inputPanel.add(lnInput, gbc);
+      gbc.gridy++;
+      inputPanel.add(entryInput, gbc);
+      gbc.gridy++;
+      inputPanel.add(ageOrDOBInput, gbc);
+      gbc.gridy++;
+      
+      inputPanel.add(genderComboBox, gbc);
+      gbc.gridy++;
+      inputPanel.add(dietCheckBox, gbc);
+      gbc.gridx++;
+      dietComboBox.setEnabled(false);
+      inputPanel.add(dietComboBox, gbc);
+      gbc.gridy++;
+      gbc.gridx--;
+      gbc.fill = GridBagConstraints.BOTH;
+      inputPanel.add(new JScrollPane(commentInput), gbc);
+
+
+      // Add labels and inputs for medical records
+      gbc.gridy++;
+      gbc.gridx = 0;
+      inputPanel.add(new JLabel("Medical Records:"), gbc);
+      gbc.gridx++;
+      inputPanel.add(new JScrollPane(medicalRecordsTextArea), gbc);
+
+      // Add labels and inputs for family connections
+      gbc.gridy++;
+      gbc.gridx = 0;
+      inputPanel.add(new JLabel("Relative Name:"), gbc);
+      gbc.gridx++;
+      inputPanel.add(relativeInput, gbc);
+      gbc.gridy++;
+      gbc.gridx = 0;
+      inputPanel.add(new JLabel("Relationship To:"), gbc);
+      gbc.gridx++;
+      inputPanel.add(relationshipInput, gbc);
+      gbc.gridy++;
+      gbc.gridx = 0;
+      gbc.gridwidth = 2;
+      inputPanel.add(addFamilyConnectionButton, gbc);
+
+      loadGenderOptions();
+
+      return inputPanel;
+   }
+
+   /**
+    * Load Gender Combo Box with options indicated in file
+   */
+   private void loadGenderOptions() {
+      List<String> genderOptions = DisasterVictim.loadGenderOptions("edu/ucalgary/oop/GenderOptions.txt");
+      for (String option : genderOptions) {
+         genderComboBox.addItem(option);
+      }
    }
 
    /**
@@ -98,7 +236,8 @@ public class DisasterVictimGUI extends JFrame implements ActionListener, MouseLi
 
       cardPanel.add(addVictimPage, "addVictimPage");
 
-      victimInfoPanel = new VictimInfoPanel();
+      JPanel victimInfoPanel = VictimInfoPanel();
+      addVictimPage.add(victimInfoPanel, BorderLayout.CENTER);
 
       instructions = new JLabel("<html><div style='text-align: left;'>Please enter the Disaster Victim information.<br/>The following information is necessary: First name, Age/DOB, Entry Date.</div></html>");
 
@@ -114,8 +253,6 @@ public class DisasterVictimGUI extends JFrame implements ActionListener, MouseLi
 
       instructions.setHorizontalAlignment(SwingConstants.LEFT);
       headerPanel.add(instructions, BorderLayout.CENTER);
-
-      addVictimPage.add(victimInfoPanel, BorderLayout.CENTER);
 
       // go back button
       JButton backButton = new JButton("Back");
@@ -135,8 +272,6 @@ public class DisasterVictimGUI extends JFrame implements ActionListener, MouseLi
     */
    private void createEditVictimPage() {
       JPanel editVictimPage = new JPanel(new BorderLayout());
-
-      victimInfoPanel = new VictimInfoPanel();
       
       DefaultListModel<String> listModel = new DefaultListModel<>();
       for (DisasterVictim victim : disasterVictims) {
@@ -151,9 +286,9 @@ public class DisasterVictimGUI extends JFrame implements ActionListener, MouseLi
             if (selectedIndex != -1) {
                   DisasterVictim selectedVictim = disasterVictims.get(selectedIndex);
 
-                  // set existing victim's information in VictimInfoPanel fields
-                  victimInfoPanel.setFnInput(selectedVictim.getFirstName());
-                  victimInfoPanel.setLnInput(selectedVictim.getLastName());
+                  // // set existing victim's information in VictimInfoPanel fields
+                  // victimInfoPanel.setFnInput(selectedVictim.getFirstName());
+                  // victimInfoPanel.setLnInput(selectedVictim.getLastName());
 
                   cardLayout.show(cardPanel, "editVictimInfoPage");
             }
@@ -215,15 +350,24 @@ public class DisasterVictimGUI extends JFrame implements ActionListener, MouseLi
     */
    private void createDisasterVictim() {
 
-      String first = victimInfoPanel.getFnInput();
-      String last = victimInfoPanel.getLnInput().isEmpty() ? null : victimInfoPanel.getLnInput();
-      String entryDate = victimInfoPanel.getEntryInput();
-      String ageOrDOB = victimInfoPanel.getAgeOrDOBInput();
-      String gender = victimInfoPanel.getSelectedGender();
-      String comment = victimInfoPanel.getCommentInput().isEmpty() ? null : victimInfoPanel.getCommentInput();
+      String first = fnInput.getText();
+      String last = lnInput.getText().isEmpty() ? null : lnInput.getText();
+      String entryDate = entryInput.getText();
+      String ageOrDOB = ageOrDOBInput.getText();
+      String gender = (String) genderComboBox.getSelectedItem();
+      String comment = commentInput.getText().isEmpty() ? null : commentInput.getText();
 
-      DietaryRestrictions diet = victimInfoPanel.getSelectedDiet();
+      DietaryRestrictions diet = (DietaryRestrictions) dietComboBox.getSelectedItem();
       
+      System.out.println("First Name: " + first);
+      System.out.println("Last Name: " + last);
+      System.out.println("Entry Date: " + entryDate);
+      System.out.println("Age/DOB: " + ageOrDOB);
+      System.out.println("Gender: " + gender);
+      System.out.println("Comment: " + comment);
+      System.out.println("Diet: " + diet);
+      
+
       DisasterVictim victim;
 
       // is age
@@ -266,8 +410,8 @@ public class DisasterVictimGUI extends JFrame implements ActionListener, MouseLi
     */
    private boolean isValidAge(String ageOrDOB) {
       try {
-         Integer.parseInt(ageOrDOB);
-         return true;
+         int age = Integer.parseInt(ageOrDOB);
+         return age >= 0 && age <= 150;
       } catch (NumberFormatException e) {
          return false;
       }
